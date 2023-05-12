25589ddab4bc46f2d774b910552738368f63d806
uditmar24 <uditmar24@gmail.com>
2023-05-13 01:40:19 +0530

    clouser

--- closer and Async/demo2.js
+++ closer and Async/demo2.js
@@ -20,3 +20,18 @@ function test() {
 }
 
 test();
+
+//-------------
+function todo(task)
+{
+    console.log("Starting todo");
+    setTimeout(function fun(){
+        console.log("completed",task);
+
+    },5000);
+    task = "dayananda";
+    console.log("end of todo");
+}
+console.log("Starting");
+todo("assignments");
+console.log("Ending");
